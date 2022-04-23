7165333d610aaaf864b2205062e23bd44b1c7d3d
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-04-23 23:49:59 +0530

    Updated api to accept tag query in url

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -12,7 +12,6 @@ const Question = require('./../models/questionModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
-
 exports.newQuestion = catchAsync(async (req, res, next) => {
 	const { user } = req;
 
@@ -20,62 +19,74 @@ exports.newQuestion = catchAsync(async (req, res, next) => {
 		title: req.body.title,
 		description: req.body.description,
 		tags: req.body.tags,
-		user: user.id   
+		user: user.id,
 	});
 
 	res.status(200).json({
-		message: "success",
+		message: 'success',
 		data: {
-			question
-		}
+			question,
+		},
 	});
-
 });
-exports.getQuestion = catchAsync(async (req, res, next) => {
-	
-});
+exports.getQuestion = catchAsync(async (req, res, next) => {});
 exports.getAllQuestions = catchAsync(async (req, res, next) => {
-	
-	let { page, size, sort } = req.query;
+	let { page } = req.query;
+	const { size, sort, tag } = req.query;
 	const { user } = req;
 
 	if (!page) page = 1;
-	const questions = await Question.find()
-		.sort({ dateCreated: -1 })
-		.limit(parseInt(size, 10))
-		.skip(parseInt((page - 1) * size, 10))
-		.populate({
-			path: 'user answers.user'  
-		});
-       
+	let questions;
+
+	if (tag) {
+		console.log(' tag found: ', tag);
+		const query = { tags: tag };
+		questions = await Question.find(query)
+			.sort({ dateCreated: -1 })
+			.limit(parseInt(size, 10))
+			.skip(parseInt((page - 1) * size, 10))
+			.populate({
+				path: 'user answers.user',
+			});
+	} else {
+		console.log('No tag found');
+		questions = await Question.find()
+			.sort({ dateCreated: -1 })
+			.limit(parseInt(size, 10))
+			.skip(parseInt((page - 1) * size, 10))
+			.populate({
+				path: 'user answers.user',
+			});
+		
+	}
 
 	// if ((sort === 'intresting' || !sort) && user) {
 
 	// 	questions = questions.filter(question => {
 	// 		let flag = false;
 	// 		question.tags.forEach(tag => {
-				
+
 	// 			if (user.tags.includes(tag)) {
 	// 				flag = true;
 	// 			}
-				
+
 	// 		});
 	// 		return flag;
-			
+
 	// 	});
 	// }
 	//console.log(questions);
 
 	res.status(200).json({
-		message: "success",
+		message: 'success',
 		data: {
-			questions
-		}
+			questions,
+		},
 	});
 });
 exports.postAnswer = catchAsync(async (req, res, next) => {
 	const { id } = req.params;
-	const {answerBody} = req.body;
+	const { answerBody } = req.body;
 	const { user } = req;
 	console.log(answerBody, req.body);
 
@@ -89,38 +100,42 @@ exports.postAnswer = catchAsync(async (req, res, next) => {
 	question.answers.push({
 		body: answerBody,
 		date: Date.now(),
-		user: user.id
+		user: user.id,
 	});
 
 	console.log({ answerBody, user, question });
 
 	const updatedQuestion = await Question.findByIdAndUpdate(id, question, {
 		new: true,
-		runValidators: true
+		runValidators: true,
 	});
 
 	res.status(200).json({
-		message: "success",
+		message: 'success',
 		body: {
-			question: updatedQuestion
-		}
+			question: updatedQuestion,
+		},
 	});
 });
 exports.deleteQuestion = catchAsync(async (req, res, next) => {
 	const { id } = req.params;
 	const { user } = req;
-	
+
 	const question = await Question.findById(id);
-	
+
 	if (`${user._id}` !== `${question.user}`) {
-		return next(new AppError("You donot have permission to perform this action", 403));
+		return next(
+			new AppError(
+				'You donot have permission to perform this action',
+				403
+			)
+		);
 	}
 	await Question.findByIdAndDelete(id);
 
 	res.status(200).json({
-		message: "success"
+		message: 'success',
 	});
-
 });
 exports.deleteAnswer = catchAsync(async (req, res, next) => {
 	const { questionId, answerId } = req.params;
@@ -128,9 +143,11 @@ exports.deleteAnswer = catchAsync(async (req, res, next) => {
 
 	const question = await Question.findById(questionId);
 	//console.log(question.user, user._id, typeof question.user, typeof user._id);
-	const answer = question.answers.find(ans => `${answerId}` === `${ans.id}`);
+	const answer = question.answers.find(
+		(ans) => `${answerId}` === `${ans.id}`
+	);
 	if (!answer) {
-		return next(new AppError("No such answer found", 404));
+		return next(new AppError('No such answer found', 404));
 	}
 	if (`${user._id}` !== `${answer.user}`) {
 		return next(
@@ -148,15 +165,17 @@ exports.deleteAnswer = catchAsync(async (req, res, next) => {
 	res.status(200).json({
 		message: 'success',
 	});
-})
+});
 exports.voteQuestion = catchAsync(async (req, res, next) => {
 	const { id, vote } = req.params;
 	const { user } = req;
 	const question = await Question.findById(id);
 	console.log(typeof vote);
-	if(vote === "upVote"){
+	if (vote === 'upVote') {
 		if (question.upVotes.includes(user.id)) {
-			return next(new AppError("ALready upVoted once. Cannot do it again"));
+			return next(
+				new AppError('ALready upVoted once. Cannot do it again')
+			);
 		}
 		const index = question.downVotes.indexOf(user.id);
 		console.log(question.upVotes.length - question.downVotes.length);
@@ -191,9 +210,11 @@ exports.voteAnswer = catchAsync(async (req, res, next) => {
 	const { questionId, vote, answerId } = req.params;
 	const { user } = req;
 	const question = await Question.findById(questionId);
-	const answer = question.answers.find(ans => `${answerId}` === `${ans.id}`);
+	const answer = question.answers.find(
+		(ans) => `${answerId}` === `${ans.id}`
+	);
 	if (!answer) {
-		return next(new AppError("No such Answer found", 404));
+		return next(new AppError('No such Answer found', 404));
 	}
 	const answerIndex = question.answers.indexOf(answer);
 	if (vote === 'upVote') {
